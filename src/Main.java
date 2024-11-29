b54110cff0086017bcc214d8b8f006da9e092906
PuaMR <paumocrec@alu.edu.gva.es>
2024-11-29 13:35:57 +0100

    Cambios de mierda (13:35 29/11/2024)

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-
-
+        // Array para el tamaño de los barcos. solo pueden haber 5 barcos de estos tamaños específicos
+        int[] barcos = {5, 4 ,3 , 3, 2};
+        char[][] JugadorTablero = new char[10][10];
+        char[][] BarcosTablero = new char[10][10];
+        int MAX_SHOTS = 32;
+        // para hacer que los barcos se pongan vertical u horizontal puedo hacer que se elija un 0
+        // o un 1, siendo 0 horizontal y 1 vertical. Luego proceder a la  colocación del barco
     }
 }
