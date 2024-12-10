868aabea71fc9b40d024b56f46ebfdee0b7aab92
PuaMR <paumocrec@alu.edu.gva.es>
2024-12-10 13:53:19 +0100

    Cambios de mierda (13:35 29/11/2024)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,63 @@
 import java.util.Scanner;
 import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        // Array para el tamaño de los barcos. solo pueden haber 5 barcos de estos tamaños específicos
-        int[] barcos = {5, 4 ,3 , 3, 2};
-        char[][] JugadorTablero = new char[10][10];
-        char[][] BarcosTablero = new char[10][10];
         int MAX_SHOTS = 32;
-        // para hacer que los barcos se pongan vertical u horizontal puedo hacer que se elija un 0
-        // o un 1, siendo 0 horizontal y 1 vertical. Luego proceder a la  colocación del barco
+        //           tamaño de los barcos:
+        //              0  1  2  3  4
+        int[] barcos = {5, 4, 3, 3, 2};
+        char[][] jugadorTablero = new char[10][10];
+        int[][] barcosTablero = new int[10][10];
+
+        // aqui se rellena el tablero que se le va a mostrar al jugador
+        for (int i = 0; i < jugadorTablero.length; i++) {
+            for (int j = 0; j < jugadorTablero[i].length; j++) {
+                jugadorTablero[i][j] = '.';
+
+            }
+        }
+
+        // aqui se rellena el tablero donde van a colocarse los barcos
+        for (int i = 0; i < barcosTablero.length; i++) {
+            for (int j = 0; j < barcosTablero[i].length; j++) {
+                barcosTablero[i][j] = -1;
+            }
+        }
+
+        // aqui se colocan los barcos en el tablero. Se establece la dirección en la que se colocara el barco (vertical 1 u horizontal 2) y las coordenadas.
+        for (int i = 0; i < barcos.length; i++) {
+            boolean puedeColocarse = false;
+            while (puedeColocarse = false) {
+                int posicion = random.nextInt(1, 2);
+                int xbarco = random.nextInt(0, 9);
+                int ybarco = random.nextInt(0, 9);
+
+                if (xbarco + barcos[i] - 1 < barcosTablero[i].length && posicion == 2) {
+                    puedeColocarse = true;
+                    for (int j = 0; j < barcos[i]; j++) {
+                        barcosTablero[ybarco][xbarco+j] = i;
+                    }
+                } else if (ybarco + barcos[i] - 1 < barcosTablero.length && posicion == 1) {
+                    for (int j = 0; j < barcos[i]; j++) {
+                        barcosTablero[ybarco+j][xbarco] = i;
+                    }
+                    puedeColocarse = true;
+                }
+
+            }
+
+        }
+
+        for (int i = 0; i < barcosTablero.length; i++) {
+            for (int j = 0; j < barcosTablero[i].length; j++) {
+                System.out.print(barcosTablero[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
